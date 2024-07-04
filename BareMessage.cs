517cc8a7a6dcb0d4466e906563d9ce82db3159e7
Verfranc <vitruvius9@hotmail.com>
2024-07-04 16:51:55 -0400

    Show message from configuration

---
+++ BareMessage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BareConsole;
+
+public class BareMessage (IConfiguration configuration)
+{
+    public void Run()
+    {
+        Console.WriteLine(configuration["Message"]);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using BareConsole;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<BareMessage>();
+    });
+var host = builder.Build();
+var bareMessage = host.Services.GetRequiredService<BareMessage>();
+
+bareMessage.Run();
+
+Console.WriteLine("Press Enter key to continue...");
+Console.ReadLine();
